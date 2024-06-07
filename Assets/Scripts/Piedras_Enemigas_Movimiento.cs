e61f12483a895165fa4e4d31f63219d86e80c766
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2024-06-07 00:53:47 -0300

    post_clase_grego

--- Assets/Scripts/Piedras_Enemigas_Movimiento.cs
+++ Assets/Scripts/Piedras_Enemigas_Movimiento.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piedras_Enemigas_Movimiento : MonoBehaviour
+public class Piedras_Enemigas_Movimiento : MonoBehaviour, IDaño
 {
     public float speed = 5f; // Velocidad del movimiento del fondo
     public Vector3 endPosition; // Posición final del segmento.
+    public float vida = 5;
 
     void Update()
     {
@@ -14,6 +15,14 @@ public class Piedras_Enemigas_Movimiento : MonoBehaviour
 
         // Si el segmento llega a la posición final, lo destruye
         if (transform.position.x <= endPosition.x)
+        {
+            Destroy();
+        }
+    }
+    public void RecibirDaño(float damage)
+    {
+        vida -= damage;
+        if (vida <= 0)
         {
             Destroy();
         }
--- Assets/Scripts/Proyectiles_Mecánicas.cs
+++ Assets/Scripts/Proyectiles_Mecánicas.cs
@@ -6,6 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 20f; // Velocidad de la bala
     public float lifeTime = 5f; // Tiempo de vida de la bala antes de ser destruida
+    public float damage = 5f;
 
     void Start()
     {
@@ -17,5 +18,28 @@ public class NewBehaviourScript : MonoBehaviour
     {
         // Mueve la bala hacia adelante en línea recta
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Verifica si la bala colisionó con un objeto con el tag "Enemigo"
+        if (other.CompareTag("Enemy"))
+        {
+            // Intenta obtener el componente IDamageable del objeto colisionado
+            IDaño damageable = other.GetComponent<IDaño>();
+
+            // Si el objeto colisionado implementa la interfaz IDamageable, llama a su método RecibirDaño
+            if (damageable != null)
+            {
+                damageable.RecibirDaño(damage);
+            }
+
+            // Destruye la bala
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
